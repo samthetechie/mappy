551709c4fa521463d5ae2a4285aed6a335d98d49
Samuel Carlisle <samuelcarlisle@gmail.com>
2012-04-12 05:25:45 +0000

    Implemented all of the buttons on the UI. Using console.log in order to test the interface.

--- mappy/static/js/map.js
+++ mappy/static/js/map.js
@@ -4,10 +4,14 @@
 var map,
     popup,
     features = {},
-    geojsonLayer = new L.GeoJSON();
+    geojsonLayer = new L.GeoJSON(),
+    bMarkerMode = false,
+    bPolylineMode = false,
+    bPolygonMode = false;
 
 $(function() {
     initMap();
+    initButtons();
 
     getAllFeatures();
 
@@ -23,6 +27,125 @@ $(function() {
         }
     });
 });
+
+
+function initButtons(){
+    //marker
+    $('#markerdefine').toggle(function() {
+        bMarkerMode = true;
+        bPolylineMode = false;
+        bPolygonMode = false;
+        console.log("Marker Mode Enabled");
+    }, function() {
+        bMarkerMode = false;
+        bPolylineMode = false;
+        bPolygonMode = false;        
+        console.log("Marker Mode Disabled");
+    });
+
+    //polyline
+    $('#polylinedefine').toggle(function() {
+        bMarkerMode = false;
+        bPolylineMode = true;
+        bPolygonMode = false;
+        console.log("Polyline Mode Enabled");
+    }, function() {
+        bMarkerMode = false;
+        bPolylineMode = false;
+        bPolygonMode = false;
+        console.log("Polyline Mode Disabled");
+    });
+
+    //polygon
+    $('#polygondefine').toggle(function() {
+        bMarkerMode = false;
+        bPolylineMode = false;
+        bPolygonMode = true;
+        console.log("Polygon Mode Enabled");
+    }, function() {
+        bMarkerMode = false;
+        bPolylineMode = false;
+        bPolygonMode = false;
+        console.log("Polygon Mode Disabled");
+    });
+    
+    $('#markerlist').click(function() {
+        console.log("Marker List Clicked");   //only fires when an option is chosen from this list!
+    });
+    
+    $('#polylinelist').click(function() {
+        console.log("Polyline List Clicked"); //only fires when an option is chosen from this list!
+    });  
+    
+    $('#polygonlist').click(function() {
+        console.log("Polygon List Clicked");  //only fires when an option is chosen from this list!
+    });
+
+    //marker buttons    
+    $('#markerzoom').click(function() {
+        console.log("Marker Zoom Clicked"); 
+    });
+      
+    $('#markerpan').click(function() {
+        console.log("Marker Pan Clicked"); 
+    });
+
+    $('#markeredit').click(function() {
+        console.log("Marker Edit Clicked"); 
+    });
+
+    $('#markerrename').click(function() {
+        console.log("Marker Rename Clicked"); 
+    });              
+
+    $('#markerremove').click(function() {
+        console.log("Marker Remove Clicked"); 
+    });
+    
+    //polyline buttons    
+    $('#polylinezoom').click(function() {
+        console.log("Polyline Zoom Clicked"); 
+    });
+      
+    $('#polylinepan').click(function() {
+        console.log("Polyline Pan Clicked"); 
+    });
+
+    $('#polylineedit').click(function() {
+        console.log("Polyline Edit Clicked"); 
+    });
+
+    $('#polylinerename').click(function() {
+        console.log("Polyline Rename Clicked"); 
+    });              
+
+    $('#polylineremove').click(function() {
+        console.log("Polyline Remove Clicked"); 
+    });
+    
+    //polygon buttons    
+    $('#polygonzoom').click(function() {
+        console.log("Polygon Zoom Clicked"); 
+    });
+      
+    $('#polygonpan').click(function() {
+        console.log("Polygon Pan Clicked"); 
+    });
+
+    $('#polygonedit').click(function() {
+        console.log("Polygon Edit Clicked"); 
+    });
+
+    $('#polygonrename').click(function() {
+        console.log("Polygon Rename Clicked"); 
+    });              
+
+    $('#polygonremove').click(function() {
+        console.log("Polygon Remove Clicked"); 
+    });
+        
+
+}
 
 function initMap() {
     map = new L.Map('map');
@@ -72,13 +195,20 @@ function getAllFeatures() {
 }
 
 function onMapClick(e) {
-    popup = new L.Popup();
-    var template = $('#template_feature_form').html();
-    var content = Mustache.to_html(template, {'lon': e.latlng.lng.toFixed(4), 
-                                              'lat': e.latlng.lat.toFixed(4)});
-    popup.setLatLng(e.latlng);
-    popup.setContent(content);
-    map.openPopup(popup);
+
+    if (bMarkerMode){
+    console.log("bMarkerMode is True and the map was clicked");
+        popup = new L.Popup();
+        var template = $('#template_feature_form').html();
+        var content = Mustache.to_html(template, {'lon': e.latlng.lng.toFixed(4), 
+                                                  'lat': e.latlng.lat.toFixed(4)});
+        popup.setLatLng(e.latlng);
+        popup.setContent(content);
+        map.openPopup(popup);    
+    }
+    else{
+    console.log("bMarkerMode is False and the map was clicked");
+    }
 }
 
 function addFeature(form) {
